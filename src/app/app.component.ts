2463eeb4196674408e4aa06419fed321d272111e
LazovoyOV <axaptastb@gmail.com>
2021-12-15 13:37:58 +0200

    счетчик app.component 15122021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zabirayservice-frontNg';
+
+  increase() {
+
+  }
+  decrease() {
+
+  }
+  clear() {
+
+  }
+
 }
